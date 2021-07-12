b5f5d9f27e04dba0191aa9e3c91173f33d47844e
maximkrouk <maximkrouk@users.noreply.github.com>
2021-07-12 16:12:47 +0000

    ci: swift-format

--- Tests/DeclarativeConfigurationTests/BuilderTests.swift
+++ Tests/DeclarativeConfigurationTests/BuilderTests.swift
@@ -1,95 +1,96 @@
 import XCTest
+
 @testable import FunctionalBuilder
 
 final class BuilderTests: XCTestCase {
-    func testBuilder() {
-        struct TestBuildable: Equatable {
-            struct Wrapped: Equatable {
-                var value = 0
-            }
-            
-            var value = false
-            var wrapped = Wrapped()
-        }
-        
-        let expected: TestBuildable = {
-            var test = TestBuildable()
-            test.value = true
-            test.wrapped.value = 1
-            return test
-        }()
-        
-        let actual = Builder(TestBuildable())
-            .wrapped.value(1)
-            .value(true)
-            .build()
-        
+  func testBuilder() {
+    struct TestBuildable: Equatable {
+      struct Wrapped: Equatable {
+        var value = 0
+      }
+
+      var value = false
+      var wrapped = Wrapped()
+    }
+
+    let expected: TestBuildable = {
+      var test = TestBuildable()
+      test.value = true
+      test.wrapped.value = 1
+      return test
+    }()
+
+    let actual = Builder(TestBuildable())
+      .wrapped.value(1)
+      .value(true)
+      .build()
+
+    XCTAssertNotEqual(actual, TestBuildable())
+    XCTAssertEqual(actual, expected)
+  }
+
+  func testReinforce() {
+    struct TestBuildable: Equatable {
+      struct Wrapped: Equatable {
+        var value = 0
+      }
+
+      var value = false
+      var wrapped = Wrapped()
+    }
+
+    let expected: TestBuildable = {
+      var test = TestBuildable()
+      test.wrapped.value = 1
+      return test
+    }()
+
+    var flag = false
+
+    _ = Builder(TestBuildable())
+      .wrapped.value(1)
+      .reinforce { actual in
+        flag = true
         XCTAssertNotEqual(actual, TestBuildable())
         XCTAssertEqual(actual, expected)
+      }
+
+    XCTAssertEqual(flag, false, "Reinforce transform wasn't called")
+  }
+
+  func testScope() {
+    struct Container: BuilderProvider {
+      class Content {
+        var a: Int = 0
+        var b: Int = 0
+        var c: Int = 0
+
+        init() {}
+      }
+
+      let content: Content = .init()
     }
-    
-    func testReinforce() {
-        struct TestBuildable: Equatable {
-            struct Wrapped: Equatable {
-                var value = 0
-            }
-            
-            var value = false
-            var wrapped = Wrapped()
-        }
-        
-        let expected: TestBuildable = {
-            var test = TestBuildable()
-            test.wrapped.value = 1
-            return test
-        }()
-        
-        var flag = false
-        
-        _ = Builder(TestBuildable())
-            .wrapped.value(1)
-            .reinforce { actual in
-                flag = true
-                XCTAssertNotEqual(actual, TestBuildable())
-                XCTAssertEqual(actual, expected)
-            }
-        
-        XCTAssertEqual(flag, false, "Reinforce transform wasn't called")
-    }
-    
-    func testScope() {
-        struct Container: BuilderProvider {
-            class Content {
-                var a: Int = 0
-                var b: Int = 0
-                var c: Int = 0
-                
-                init() {}
-            }
-            
-            let content: Content = .init()
-        }
-        
-        let expected = Container().builder
-            .content.a(1)
-            .content.b(2)
-            .content.c(3)
-            .build()
-        let initial = Container()
-        let actual = Container().builder
-            .content.scope { $0
-                .a(1)
-                .b(2)
-                .c(3)
-            }.build()
-        
-        
-        XCTAssertNotEqual(actual.content.a, initial.content.a)
-        XCTAssertNotEqual(actual.content.b, initial.content.b)
-        XCTAssertNotEqual(actual.content.c, initial.content.c)
-        
-        XCTAssertEqual(actual.content.a, expected.content.a)
-        XCTAssertEqual(actual.content.b, expected.content.b)
-        XCTAssertEqual(actual.content.c, expected.content.c)
-    }
+
+    let expected = Container().builder
+      .content.a(1)
+      .content.b(2)
+      .content.c(3)
+      .build()
+    let initial = Container()
+    let actual = Container().builder
+      .content.scope {
+        $0
+          .a(1)
+          .b(2)
+          .c(3)
+      }.build()
+
+    XCTAssertNotEqual(actual.content.a, initial.content.a)
+    XCTAssertNotEqual(actual.content.b, initial.content.b)
+    XCTAssertNotEqual(actual.content.c, initial.content.c)
+
+    XCTAssertEqual(actual.content.a, expected.content.a)
+    XCTAssertEqual(actual.content.b, expected.content.b)
+    XCTAssertEqual(actual.content.c, expected.content.c)
+  }
 }
--- Tests/DeclarativeConfigurationTests/ModificationTests.swift
+++ Tests/DeclarativeConfigurationTests/ModificationTests.swift
@@ -1,50 +1,51 @@
 import XCTest
+
 @testable import FunctionalModification
 
 final class ModificationTests: XCTestCase {
-    func testValueModificationForValueTypes() {
-        struct _Test { var value = 0 }
-        let initial = _Test(value: 0)
-        let expected = _Test(value: 1)
-        let actual = modification(of: initial) { $0.value = expected.value }
-        XCTAssertNotEqual(initial.value, expected.value)
-        XCTAssertEqual(actual.value, expected.value)
-    }
-    
-    func testInstanceModificationForValueTypes() {
-        struct _Test { var value = 0 }
-        let initial = _Test(value: 0)
-        let expected = _Test(value: 1)
-        let actual = modification(of: initial) { $0 = expected }
-        XCTAssertNotEqual(initial.value, expected.value)
-        XCTAssertEqual(actual.value, expected.value)
-    }
-    
-    func testValueModificationForReferenceTypes() {
-        class _Test { var value = 0 }
-        let initial = _Test()
-        let expected: _Test = {
-            let test = _Test()
-            test.value = 1
-            return test
-        }()
-        
-        let actual = modification(of: initial) { $0.value = expected.value }
-        XCTAssertEqual(ObjectIdentifier(actual), ObjectIdentifier(initial))
-        XCTAssertEqual(actual.value, expected.value)
-    }
-    
-    func testInstanceModificationForReferenceTypes() {
-        class _Test { var value = 0 }
-        let initial = _Test()
-        let expected: _Test = {
-            let test = _Test()
-            test.value = 1
-            return test
-        }()
-        
-        let actual = modification(of: initial) { $0 = expected }
-        XCTAssertNotEqual(ObjectIdentifier(initial), ObjectIdentifier(expected))
-        XCTAssertEqual(ObjectIdentifier(actual), ObjectIdentifier(expected))
-    }
+  func testValueModificationForValueTypes() {
+    struct _Test { var value = 0 }
+    let initial = _Test(value: 0)
+    let expected = _Test(value: 1)
+    let actual = modification(of: initial) { $0.value = expected.value }
+    XCTAssertNotEqual(initial.value, expected.value)
+    XCTAssertEqual(actual.value, expected.value)
+  }
+
+  func testInstanceModificationForValueTypes() {
+    struct _Test { var value = 0 }
+    let initial = _Test(value: 0)
+    let expected = _Test(value: 1)
+    let actual = modification(of: initial) { $0 = expected }
+    XCTAssertNotEqual(initial.value, expected.value)
+    XCTAssertEqual(actual.value, expected.value)
+  }
+
+  func testValueModificationForReferenceTypes() {
+    class _Test { var value = 0 }
+    let initial = _Test()
+    let expected: _Test = {
+      let test = _Test()
+      test.value = 1
+      return test
+    }()
+
+    let actual = modification(of: initial) { $0.value = expected.value }
+    XCTAssertEqual(ObjectIdentifier(actual), ObjectIdentifier(initial))
+    XCTAssertEqual(actual.value, expected.value)
+  }
+
+  func testInstanceModificationForReferenceTypes() {
+    class _Test { var value = 0 }
+    let initial = _Test()
+    let expected: _Test = {
+      let test = _Test()
+      test.value = 1
+      return test
+    }()
+
+    let actual = modification(of: initial) { $0 = expected }
+    XCTAssertNotEqual(ObjectIdentifier(initial), ObjectIdentifier(expected))
+    XCTAssertEqual(ObjectIdentifier(actual), ObjectIdentifier(expected))
+  }
 }
