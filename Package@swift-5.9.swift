d8fe768889f5fe7cdfedb4fcea59af4f86c74c11
maximkrouk <id.maximkrouk@gmail.com>
2026-02-05 06:04:02 +0100

    feat: Update dependencies

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -47,7 +47,7 @@ let package = Package(
 	dependencies: [
 		.package(
 			url: "https://github.com/capturecontext/swift-keypaths-extensions.git",
-			.upToNextMinor(from: "0.1.4")
+			.upToNextMinor(from: "0.1.6")
 		),
 		.package(
 			url: "https://github.com/pointfreeco/xctest-dynamic-overlay.git",
